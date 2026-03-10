1427a264ac2c9291d037cd0ce73fba7c910439fb
Shishmakova Daria <dary_shishka@mail.ru>
2026-03-11 00:02:34 +0300

    Initial Commit

---
+++ CatsApp/CatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppApp.swift
+//  CatsApp
+//
+//  Created by Дарья Шишмакова on 11.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatsApp
+//
+//  Created by Дарья Шишмакова on 11.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
